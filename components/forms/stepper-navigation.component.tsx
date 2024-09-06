962e580d1354f6d241736b74705e38e6d09ad334
facurzn21 <facundoeze@gmail.com>
2024-09-06 17:53:20 -0300

    fix

--- components/forms/stepper-navigation.component.tsx
+++ components/forms/stepper-navigation.component.tsx
@@ -31,7 +31,7 @@ const StepperNavigation: FC<StepperNavigationProps> = ({
           <Button
             onClick={handleNext}
             sx={{ mr: 1 }}
-            variant="nextStepBuy"
+            variant="text"
             endIcon={<ChevronRightIcon />}
           >
             SIGUIENTE
