e566b85776ee7964045326976b84ce1f90e57ff7
facurzn21 <facundoeze@gmail.com>
2024-09-08 21:14:42 -0300

    Spped in vercel

--- pages/_app.page.tsx
+++ pages/_app.page.tsx
@@ -2,6 +2,7 @@ import type { AppProps } from "next/app";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import LayoutGeneral from "dh-marvel/components/layouts/layout-general";
 import { theme } from "dh-marvel/styles/material-theme";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 function MyApp({ Component, pageProps }: AppProps) {
   const LayoutComponent = (Component as any).Layout;
@@ -19,6 +20,7 @@ function MyApp({ Component, pageProps }: AppProps) {
           <Component {...pageProps} />
         </LayoutGeneral>
       )}
+      <SpeedInsights /> 
       <style jsx global>{`
         #__next {
           height: 100%;
